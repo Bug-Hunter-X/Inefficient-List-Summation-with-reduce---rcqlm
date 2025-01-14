ae697c44d67c1f6c150c63186d23603d1f9f4e60
BugHunter-X <askzenai@gmail.com>
2025-01-14 11:12:04 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,7 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int sum = numbers.reduce((a, b) => a + b);
+print(sum); // Output: 15
+
+//This code is fine but it can be written in a more efficient way using a for loop. 
+```
